f9d8826964694e2de6f81eaf96b9d627df2fc7a8
nsx <1743091664@qq.com>
2022-08-18 16:52:31 +0800

    fix: fix themeSwitcher bug

--- src/components/themeSwitcher/index.js
+++ src/components/themeSwitcher/index.js
@@ -1,15 +1,16 @@
 import React, { memo, useContext } from 'react'
 import { FiMoon, FiSun } from 'react-icons/fi'
 import ThemeContext from '../../context/themeContext'
+import themeList from '../../theme/themeList'
 
 import ThemeSwitcherStyle from './style'
 
 const ThemeSwitcher = memo(() => {
-  const { toggleTheme } = useContext(ThemeContext)
+  const { theme, toggleTheme } = useContext(ThemeContext)
 
   return (
     <ThemeSwitcherStyle>
-      <input type="checkbox" id="switcher" onChange={toggleTheme} />
+      <input type="checkbox" id="switcher" onChange={toggleTheme} checked={theme === themeList.dark} />
       <label htmlFor="switcher" >
         <div className="icon">
           <FiSun />
